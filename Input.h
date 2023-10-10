8ccf30923f6c7e2c7863967446768445d2fe563a
HamamatuReiya <115063040+HamamatuReiya@users.noreply.github.com>
2023-10-10 15:28:29 +0900

    終了

--- Input.h
+++ Input.h
@@ -1,6 +1,14 @@
 #pragma once
 #include <Windows.h>
 
+#define DIRECTINPUT_VERSION     0x0800   // DirectInputのバージョン指定
+#include <dinput.h>
+
+#pragma comment(lib, "dinput8.lib")
+#pragma comment(lib, "dxguid.lib")
+
+#include <wrl.h>
+
 class Input
 {
 public://メンバ関数
@@ -8,5 +16,8 @@ public://メンバ関数
 	void Initialize(HINSTANCE hInstance,HWND hwnd);
 	//更新
 	void Update();
+
+private:
+	Microsoft::WRL::ComPtr<IDirectInputDevice8> keyboard;
 };
 

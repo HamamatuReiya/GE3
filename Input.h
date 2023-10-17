a31c744a88fcd49da84824e287e92ce0d0a031e2
HamamatuReiya <115063040+HamamatuReiya@users.noreply.github.com>
2023-10-17 15:17:00 +0900

    完了

--- Input.h
+++ Input.h
@@ -17,7 +17,17 @@ public://メンバ関数
 	//更新
 	void Update();
 
+	//任意のボタンを押されている
+	bool PushKey(BYTE keyNumber);
+	//任意のボタンが押された瞬間
+	bool TriggerKey(BYTE keyNumber);
+	//任意のボタンが離された瞬間
+
 private:
 	Microsoft::WRL::ComPtr<IDirectInputDevice8> keyboard;
+	Microsoft::WRL::ComPtr<IDirectInput8> directInput;
+
+	BYTE key[256] = {};
+	BYTE keyPre[256] = {};
 };
 

024a034897c26a181e6ae9d8b6aa6ad1c5484081
HamamatuReiya <115063040+HamamatuReiya@users.noreply.github.com>
2024-02-13 20:58:52 +0900

    途中

--- Sprite.cpp
+++ Sprite.cpp
@@ -16,10 +16,48 @@ void Sprite::Initialize(DirectXCommon* dxCommon, SpriteCommon* common)
 	CreateVertex();
 	// 色
 	CreateMaterial();
+	// 行列
+	CreateWVP();
 }
 
 void Sprite::Draw()
 {
+	// Y軸中心に回転
+	transform.rotate.y += 0.03f;
+	// ワールド
+	XMMATRIX scaleMatrix = XMMatrixScalingFromVector(XMLoadFloat3(&transform.scale));
+	XMMATRIX rotateMatrix = XMMatrixRotationRollPitchYawFromVector(XMLoadFloat3(&transform.rotate));
+	XMMATRIX translationMatrix = XMMatrixTranslationFromVector(XMLoadFloat3(&transform.translate));
+	// 回転行列とスケール行列の掛け算
+	XMMATRIX rotationAndScaleMatrix = XMMatrixMultiply(rotateMatrix, scaleMatrix);
+	// 最終的な行列変換
+	XMMATRIX worldMatrix = XMMatrixMultiply(rotationAndScaleMatrix, translationMatrix);
+
+	// カメラ
+	XMMATRIX cameraScaleMatrix = XMMatrixScalingFromVector(XMLoadFloat3(&cameraTransform.scale));
+	XMMATRIX cameraRotateMatrix = XMMatrixRotationRollPitchYawFromVector(XMLoadFloat3(&cameraTransform.rotate));
+	XMMATRIX cameraTranslationMatrix = XMMatrixTranslationFromVector(XMLoadFloat3(&cameraTransform.translate));
+	// 回転とスケールの掛け算
+	XMMATRIX cameraRotateAndScameMatrix = XMMatrixMultiply(cameraRotateMatrix, cameraScaleMatrix);
+	// 最終的な行列
+	XMMATRIX cameraMatrix = XMMatrixMultiply(cameraRotateAndScameMatrix, cameraTranslationMatrix);
+
+	// View
+	XMMATRIX view = XMMatrixInverse(nullptr, cameraMatrix);
+	// Proj
+	XMMATRIX proj = XMMatrixPerspectiveFovLH(
+		XMConvertToRadians(45.f),
+		(float)WinApp::window_width / (float)WinApp::window_height,
+		0.1f,
+		100.0f
+	);
+
+	// WVP
+	XMMATRIX worldViewProjecionMatrix = worldMatrix * (view * proj);
+
+	// 行列の代入
+	*wvpData = worldViewProjecionMatrix;
+
 	dxCommon_->GetCommandList()->SetGraphicsRootSignature(common_->GetRootSignature());
 	dxCommon_->GetCommandList()->SetPipelineState(common_->GetPipelineState());
 	
@@ -30,6 +68,8 @@ void Sprite::Draw()
 
 	// 色情報
 	dxCommon_->GetCommandList()->SetGraphicsRootConstantBufferView(0, materialRsource->GetGPUVirtualAddress());
+	// 行列
+	dxCommon_->GetCommandList()->SetGraphicsRootConstantBufferView(1, wvpResource->GetGPUVirtualAddress());
 
 	dxCommon_->GetCommandList()->DrawInstanced(3, 1, 0, 0);
 }
@@ -60,4 +100,13 @@ void Sprite::CreateMaterial()
 	materialRsource->Map(0, nullptr, reinterpret_cast<void**>(&materialData));
 
 	*materialData = XMFLOAT4(1.0f, 0.0f, 0.0f, 1.0f);
+}
+
+void Sprite::CreateWVP()
+{
+	wvpResource = CreateBufferResource(dxCommon_->GetDevice(), sizeof(XMMATRIX));
+
+	wvpResource->Map(0, nullptr, reinterpret_cast<void**>(&wvpData));
+
+	*wvpData = XMMatrixIdentity();
 }
--- Sprite.h
+++ Sprite.h
@@ -3,6 +3,8 @@
 #include <d3d12.h>
 #include <wrl.h>
 
+#include <DirectXMath.h>
+
 #include "SpriteCommon.h"
 
 //単体
@@ -10,6 +12,13 @@ class Sprite
 {
 private:
 	template <class T> using ComPtr = Microsoft::WRL::ComPtr<T>;
+
+private:
+	struct Transform{
+		DirectX::XMFLOAT3 scale;
+		DirectX::XMFLOAT3 rotate;
+		DirectX::XMFLOAT3 translate;
+	};
 
 public:
 	void Initialize(DirectXCommon* dxCommon,SpriteCommon* common);
@@ -21,6 +30,8 @@ private:
 	void CreateVertex();
 	// マテリアル情報作成
 	void CreateMaterial();
+	// 行列情報作成
+	void CreateWVP();
 
 private:
 	DirectXCommon* dxCommon_ = nullptr;
@@ -32,5 +43,17 @@ private:
 
 	// マテリアル情報
 	ComPtr<ID3D12Resource> materialRsource;
+
+	// 行列情報
+	ComPtr<ID3D12Resource> wvpResource;
+	DirectX::XMMATRIX* wvpData = nullptr;
+
+	// パラメーター
+	DirectX::XMFLOAT4 color_{ 1.0f,0.0f,0.0f,1.0f };
+	//                       Scale   Rotate Translate
+	Transform transform = { {1,1,1},{0,0,0},{0,0,0} };
+
+	// カメラ
+	Transform cameraTransform = { {1,1,1},{0,0,0},{0,0,-5} };
 };
 
